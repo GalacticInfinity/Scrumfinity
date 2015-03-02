47a47cc0dbd5aeef44985fe49224ac9887b5a81c
Michael Roman <msr51@canterbury.ac.nz>
2015-03-02 13:03:25 +1300

    Cant find src

---
+++ src/main/java/testpackage/Main.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+public class Main {
+
+  public static void main(String[] args) {
+    sayHello();
+  }
+
+  private static void sayHello() {
+    System.out.println("Hello World!");
+    System.out.println("Hello World3!");
+  }
+}
