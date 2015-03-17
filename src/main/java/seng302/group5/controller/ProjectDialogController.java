6058a6a4f9106654f47d47109bad6a21d5031c0e
Su-Shing Chen <ssc55@uclive.ac.nz>
2015-03-17 19:55:25 +1300

    Creation of projects now has case handling for names
    Checks the ID and project name if they are empty or too long, and output the errors in stdout if there are errors, to be later placed in dialog messages. Also refactored some dialog field IDs to be more clear.
    
    story:95

--- src/main/java/seng302/group5/controller/ProjectDialogController.java
+++ src/main/java/seng302/group5/controller/ProjectDialogController.java
@@ -1,6 +1,5 @@
 package seng302.group5.controller;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -11,26 +10,92 @@ import seng302.group5.Main;
 /**
  * Created by @author Alex Woo
  */
-
-
-
 public class ProjectDialogController {
 
-  @FXML private TextField shortName;
-  @FXML private TextField projectName;
-  @FXML private TextArea projectDescription;
+  @FXML private TextField projectIDField;
+  @FXML private TextField projectNameField;
+  @FXML private TextArea projectDescriptionField;
 
   private Main mainApp;
   private Stage thisStage;
+
+  /**
+   * Parse a string containing a project ID. Throws exceptions if input is not valid.
+   * @param inputProjectID String of project ID
+   * @return Inputted project ID if it is valid.
+   * @throws Exception Exception with message explaining why input is invalid.
+   */
+  private String parseProjectID(String inputProjectID) throws Exception {
+    inputProjectID = inputProjectID.trim();
+
+    if (inputProjectID.isEmpty()) {
+      throw new Exception("Project ID is empty");
+    } else if (inputProjectID.length() > 10) {
+      throw new Exception("Project ID is more than 10 characters long");
+    } else {
+      return inputProjectID;
+    }
+  }
+
+  /**
+   * Parse a string containing a project name. Throws exceptions if input is not valid.
+   * @param inputProjectName String of project name
+   * @return Inputted project name if it is valid.
+   * @throws Exception Exception with message explaining why input is invalid.
+   */
+  private String parseProjectName(String inputProjectName) throws Exception {
+    inputProjectName = inputProjectName.trim();
+
+    if (inputProjectName.isEmpty()) {
+      throw new Exception("Project Name is empty");
+    } else if (inputProjectName.length() > 16) {
+      throw new Exception("Project Name is more than 16 characters long");
+    } else {
+      return inputProjectName;
+    }
+  }
 
   /**
    * Handles when the create button is pushed
    */
   @FXML
   protected void btnCreateProjectClick(ActionEvent event) {
+    StringBuilder errors = new StringBuilder();
+    errors.append("Invalid Fields:");
+    int noErrors = 0;
 
-    mainApp.addProject(shortName.getText(), projectName.getText(), projectDescription.getText());
-    thisStage.close();
+    String projectID = "";
+    String projectName = "";
+    String projectDescription = projectDescriptionField.getText().trim();
+
+    try {
+      projectID = parseProjectID(projectIDField.getText());
+    } catch (Exception e) {
+      noErrors++;
+      errors.append(String.format("\n\t%s", e.getMessage()));
+    }
+
+    try {
+      projectName = parseProjectName(projectNameField.getText());
+    } catch (Exception e) {
+      noErrors++;
+      errors.append(String.format("\n\t%s", e.getMessage()));
+    }
+
+    // Display all errors if they exist
+    if (noErrors > 0) {
+      String title;
+      if (noErrors == 1) {
+        title = String.format("%d Invalid Field", noErrors);
+      } else {
+        title = String.format("%d Invalid Fields", noErrors);
+      }
+      // TODO: Dialogs for errors
+      System.out.println(String.format("%s\n%s", title, errors.toString()));
+    } else {
+      mainApp.addProject(projectID, projectName, projectDescription);
+      thisStage.close();
+    }
   }
 
   @FXML
@@ -38,7 +103,7 @@ public class ProjectDialogController {
     thisStage.close();
   }
 
-  public void setMainApp(Main mainApp){
+  public void setMainApp(Main mainApp) {
     this.mainApp = mainApp;
   }
 
