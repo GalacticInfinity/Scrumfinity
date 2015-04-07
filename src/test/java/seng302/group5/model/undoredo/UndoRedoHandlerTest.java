48c81aacc903fa24a820740e77470149f7dc639b
Su-Shing Chen <ssc55@uclive.ac.nz>
2015-04-07 14:06:47 +1200

    New test classes for undo
    no content yet
    
    Story:96

---
+++ src/test/java/seng302/group5/model/undoredo/UndoRedoHandlerTest.java
@@ -0,0 +1,36 @@
+package seng302.group5.model.undoredo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for undo and redo handler
+ */
+public class UndoRedoHandlerTest {
+
+  @Test
+  public void testPeekUndoStack() throws Exception {
+    // TODO
+  }
+
+  @Test
+  public void testClearStacks() throws Exception {
+    // TODO
+  }
+
+  @Test
+  public void testNewAction() throws Exception {
+    // TODO
+  }
+
+  @Test
+  public void testUndo() throws Exception {
+    // TODO
+  }
+
+  @Test
+  public void testRedo() throws Exception {
+    // TODO
+  }
+}
