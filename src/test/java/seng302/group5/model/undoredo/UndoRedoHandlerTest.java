3e3431661d08243416d4776f60c2d1838a303e4c
Su-Shing Chen <ssc55@uclive.ac.nz>
2015-04-11 14:13:46 +1200

    Undo Redo handler tests for Person deletion
    Pair[apw76]
    Story:96

--- src/test/java/seng302/group5/model/undoredo/UndoRedoHandlerTest.java
+++ src/test/java/seng302/group5/model/undoredo/UndoRedoHandlerTest.java
@@ -57,6 +57,16 @@ public class UndoRedoHandlerTest {
     undoRedoHandler.newAction(undoRedoObject);
   }
 
+  private void deleteNewestPerson() {
+    mainApp.deletePerson(person);
+
+    UndoRedoObject undoRedoObject = new UndoRedoObject();
+    undoRedoObject.setAction(Action.PERSON_DELETE);
+    undoRedoObject.addDatum(new Person(person));
+
+    undoRedoHandler.newAction(undoRedoObject);
+  }
+
   @Test
   public void testPeekUndoStack() throws Exception {
     assertTrue(undoRedoHandler.getUndoStack().empty());
@@ -112,20 +122,66 @@ public class UndoRedoHandlerTest {
   }
 
   @Test
-  public void testRedo() throws Exception {
+  public void testPersonCreateRedo() throws Exception {
     assertTrue(mainApp.getPeople().isEmpty());
-    assertTrue(undoRedoHandler.getUndoStack().empty());
+    assertTrue(undoRedoHandler.getRedoStack().empty());
 
     newPerson();
     assertFalse(mainApp.getPeople().isEmpty());
-    assertFalse(undoRedoHandler.getUndoStack().empty());
+    assertTrue(undoRedoHandler.getRedoStack().empty());
 
     undoRedoHandler.undo();
     assertTrue(mainApp.getPeople().isEmpty());
-    assertTrue(undoRedoHandler.getUndoStack().empty());
+    assertFalse(undoRedoHandler.getRedoStack().empty());
 
     undoRedoHandler.redo();
     assertFalse(mainApp.getPeople().isEmpty());
     assertFalse(undoRedoHandler.getUndoStack().empty());
+    assertTrue(undoRedoHandler.getRedoStack().empty());
+  }
+
+  @Test
+  public void testPersonDeleteUndo() throws Exception {
+    assertTrue(mainApp.getPeople().isEmpty());
+    assertTrue(undoRedoHandler.getUndoStack().empty());
+
+    newPerson();
+    assertEquals(mainApp.getPeople().size(), 1);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 1);
+
+    deleteNewestPerson();
+    assertEquals(mainApp.getPeople().size(), 0);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 2);
+
+    undoRedoHandler.undo();
+    assertEquals(mainApp.getPeople().size(), 1);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 1);
+  }
+
+  @Test
+  public void testPersonDeleteRedo() throws Exception {
+    assertTrue(mainApp.getPeople().isEmpty());
+    assertTrue(undoRedoHandler.getUndoStack().empty());
+    assertTrue(undoRedoHandler.getRedoStack().empty());
+
+    newPerson();
+    assertEquals(mainApp.getPeople().size(), 1);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 1);
+    assertTrue(undoRedoHandler.getRedoStack().empty());
+
+    deleteNewestPerson();
+    assertEquals(mainApp.getPeople().size(), 0);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 2);
+    assertTrue(undoRedoHandler.getRedoStack().empty());
+
+    undoRedoHandler.undo();
+    assertEquals(mainApp.getPeople().size(), 1);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 1);
+    assertEquals(undoRedoHandler.getRedoStack().size(), 1);
+
+    undoRedoHandler.redo();
+    assertEquals(mainApp.getPeople().size(), 0);
+    assertEquals(undoRedoHandler.getUndoStack().size(), 2);
+    assertTrue(undoRedoHandler.getRedoStack().empty());
   }
 }
