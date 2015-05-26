3cd9da9bd1a15f790f48c21148d32ce8fa63f913
Michael Roman <msr51@canterbury.ac.nz>
2015-05-27 11:55:29 +1200

    Fixed bug in file opening
    Always opened in default directory, now checks if Settings.currentFile is set, ans if so, opens file chooser there.
    
    story: 189

--- src/main/java/seng302/group5/model/util/NewSaving.java
+++ src/main/java/seng302/group5/model/util/NewSaving.java
@@ -1,4 +1,4 @@
-package seng302.group5.model;
+package seng302.group5.model.util;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -8,6 +8,14 @@ import java.io.Writer;
 import java.util.List;
 
 import seng302.group5.Main;
+import seng302.group5.model.AgileHistory;
+import seng302.group5.model.Person;
+import seng302.group5.model.Project;
+import seng302.group5.model.Release;
+import seng302.group5.model.Role;
+import seng302.group5.model.Skill;
+import seng302.group5.model.Story;
+import seng302.group5.model.Team;
 import seng302.group5.model.util.Settings;
 
 /**
