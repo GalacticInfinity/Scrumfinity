c56409316b7870f3239f36872cb793b87081db4a
Su-Shing Chen <ssc55@uclive.ac.nz>
2015-07-24 13:10:33 +1200

    Adding and removing stories from sprints in the dialog.
    Implemented functionality to add or remove stories to and from a sprint while maintaining priority order using an interesting fix. The fix involves maintaining both a set and a prioritsed list for allocated stories. On refreshing, the available stories resets itself with stories from the backlog in the original order and then removes all stories which exist in the set. The allocated stories list refreshes by resetting it self with stories from the backlog in the original order and then removes all stories which do not exist in the set.
    
    story: 235
    pair[msr51]

--- src/main/java/seng302/group5/controller/dialogControllers/SprintDialogController.java
+++ src/main/java/seng302/group5/controller/dialogControllers/SprintDialogController.java
@@ -2,6 +2,8 @@ package seng302.group5.controller.dialogControllers;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -18,7 +20,6 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import seng302.group5.Main;
 import seng302.group5.controller.enums.CreateOrEdit;
-import seng302.group5.model.AgileHistory;
 import seng302.group5.model.Backlog;
 import seng302.group5.model.Project;
 import seng302.group5.model.Release;
@@ -61,12 +62,13 @@ public class SprintDialogController {
   private Project project;
 
   private ObservableList<Story> availableStories;
-  private ObservableList<Story> allocatedStories;
+  private ObservableList<Story> allocatedStoriesPrioritised;
   private ObservableList<Backlog> backlogs;
   private ObservableList<Team> teams;
   private ObservableList<Release> releases;
 
   private Map<Backlog, Project> projectMap;
+  private Set<Story> allocatedStories;   // use to maintain priority order
 
   //todo jdoc
   public void setupController(Main mainApp, Stage thisStage, CreateOrEdit createOrEdit,
@@ -123,9 +125,8 @@ public class SprintDialogController {
   //todo jdoc
   private void initialiseLists(CreateOrEdit createOrEdit, Sprint sprint) {
     availableStories = FXCollections.observableArrayList();
-    allocatedStories = FXCollections.observableArrayList();
+    allocatedStoriesPrioritised = FXCollections.observableArrayList();
     backlogs = FXCollections.observableArrayList();
-    // todo map from backlog to project?
     teams = FXCollections.observableArrayList();
     releases = FXCollections.observableArrayList();
 
@@ -138,12 +139,17 @@ public class SprintDialogController {
       }
     }
 
+    allocatedStories = new TreeSet<>();
+
     backlogs.setAll(mainApp.getBacklogs());
 
     sprintBacklogCombo.setVisibleRowCount(5);
     sprintTeamCombo.setVisibleRowCount(5);
     sprintReleaseCombo.setVisibleRowCount(5);
     sprintBacklogCombo.setItems(backlogs);
+
+    availableStoriesList.setItems(availableStories);
+    allocatedStoriesList.setItems(allocatedStoriesPrioritised);
 
     sprintBacklogCombo.getSelectionModel().selectedItemProperty().addListener(
         (observable, oldBacklog, newBacklog) -> {
@@ -166,27 +172,71 @@ public class SprintDialogController {
               }
             }
             sprintReleaseCombo.setItems(releases);
+
+            allocatedStories.clear();
+            refreshLists();
           } else {
             // todo dialog for confirming change
             project = null;
+            sprintProjectLabel.setText("No Project Found");
             sprintTeamCombo.setValue(null);
             sprintReleaseCombo.setValue(null);
             sprintTeamCombo.setDisable(true);
             sprintReleaseCombo.setDisable(true);
+            availableStories.clear();
+            allocatedStoriesPrioritised.clear();
+            allocatedStories.clear();
           }
         });
   }
 
-  // todo jdoc
+  /**
+   * Adds the selected story from the list of available stories in the selected product backlog
+   * to the allocated stories for the sprint.
+   *
+   * @param event Action event
+   */
   @FXML
   void btnAddStoryClick(ActionEvent event) {
-
+    Story selectedStory = availableStoriesList.getSelectionModel().getSelectedItem();
+    if (selectedStory != null) {
+      allocatedStories.add(selectedStory);
+      refreshLists();
+      allocatedStoriesList.getSelectionModel().select(selectedStory);
+    }
   }
 
-  // todo jdoc
+  /**
+   * Remove the selected story from the list of allocated stories in the sprint and put it back
+   * in the list of available stories.
+   *
+   * @param event Action event
+   */
   @FXML
   void btnRemoveStoryClick(ActionEvent event) {
+    Story selectedStory = allocatedStoriesList.getSelectionModel().getSelectedItem();
+    if (selectedStory != null) {
+      allocatedStories.remove(selectedStory);
+      refreshLists();
+      availableStoriesList.getSelectionModel().select(selectedStory);
+    }
+  }
 
+  /**
+   * Refresh the lists such that they maintain the original priority order specified in the
+   * backlog. Call whenever the story allocation changes.
+   */
+  private void refreshLists() {
+    Backlog selectedBacklog = sprintBacklogCombo.getSelectionModel().getSelectedItem();
+    availableStories.setAll(selectedBacklog.getStories());  // add in priority order
+    availableStories.removeAll(allocatedStories);
+
+    allocatedStoriesPrioritised.clear();
+    for (Story story : selectedBacklog.getStories()) {
+      if (allocatedStories.contains(story)) {
+        allocatedStoriesPrioritised.add(story);
+      }
+    }
   }
 
   // todo jdoc
@@ -195,7 +245,11 @@ public class SprintDialogController {
 
   }
 
-  // todo jdoc
+  /**
+   * Closes the window.
+   *
+   * @param event Action event
+   */
   @FXML
   void btnCancelClick(ActionEvent event) {
     thisStage.close();
