4f038fe6ff8a2a4c664400d0d53df9d06b24c154
Su-Shing Chen <ssc55@uclive.ac.nz>
2015-08-12 11:54:13 +1200

    Time parser class updated with unit tests
    Wrote some tests first to confirm what I wanted as results before implementing (TDD) then wrote the converter from int to time formatted string. Parser converts a minute integer to a time in the format [x]h[y]m where x and y are integers.
    
    story:314
    pair[apw76]

--- src/test/java/seng302/group5/model/util/TimeFormatTest.java
+++ src/test/java/seng302/group5/model/util/TimeFormatTest.java
@@ -10,72 +10,107 @@ import static org.junit.Assert.*;
 public class TimeFormatTest {
 
   @Test
-  public void testEmpty() {
+  public void testEmptyString() {
     String inputString = "";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(0, result);
   }
 
   @Test
-  public void testValidHoursMinutes() {
+  public void testValidHoursMinutesString() {
     String inputString = "1h30m";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(90, result);
   }
 
   @Test
-  public void testValidLongHoursMinutes() {
+  public void testValidLongHoursMinutesString() {
     String inputString = "100h30m";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(6030, result);
   }
 
   @Test
-  public void testValidHours() {
+  public void testValidHoursString() {
     String inputString = "2h";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(120, result);
   }
 
   @Test
-  public void testValidLongHours() {
+  public void testValidLongHoursString() {
     String inputString = "150h";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(9000, result);
   }
 
   @Test
-  public void testValidMinutes() {
+  public void testValidMinutesString() {
     String inputString = "59m";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(59, result);
   }
 
   @Test
-  public void testValidLongMinutes() {
+  public void testValidLongMinutesString() {
     String inputString = "9001m";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(9001, result);
   }
 
   @Test
   public void testRandomString() {
     String inputString = "Hello World";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(-1, result);
   }
 
   @Test
-  public void testSingleCharH() {
+  public void testSingleCharHString() {
     String inputString = "h";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(-1, result);
   }
 
   @Test
-  public void testSingleCharM() {
+  public void testSingleCharMString() {
     String inputString = "m";
-    int result = TimeFormat.parseTime(inputString);
+    int result = TimeFormat.parseMinutes(inputString);
     assertEquals(-1, result);
+  }
+
+  @Test
+  public void testValidHoursMinutesInt() {
+    int inputInt = 1000;
+    String result = TimeFormat.parseTime(inputInt);
+    assertEquals("16h40m", result);
+  }
+
+  @Test
+  public void testValidHoursInt() {
+    int inputInt = 960;
+    String result = TimeFormat.parseTime(inputInt);
+    assertEquals("16h", result);
+  }
+
+  @Test
+  public void testValidMinutesInt() {
+    int inputInt = 38;
+    String result = TimeFormat.parseTime(inputInt);
+    assertEquals("38m", result);
+  }
+
+  @Test
+  public void testZeroMinutesInt() {
+    int inputInt = 0;
+    String result = TimeFormat.parseTime(inputInt);
+    assertEquals("0m", result);
+  }
+
+  @Test
+  public void testInvalidMinutesInt() {
+    int inputInt = -1;
+    String result = TimeFormat.parseTime(inputInt);
+    assertEquals("", result);
   }
 }
