fade9c170d9c38f862164e5a52d1f01dee82daef
Michael Roman <msr51@canterbury.ac.nz>
2015-09-11 10:41:45 +1200

    Mainly doing research, but now have base code for drag and drop. Instead of using listView, using vBox with labels. Will need to evntaully style to make it look like all other lists or put more effort and create "cards". Will now work on dragging across different vboxes.
    story:360

--- src/main/java/seng302/group5/controller/dialogControllers/StoryItemController.java
+++ src/main/java/seng302/group5/controller/dialogControllers/StoryItemController.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import seng302.group5.model.Story;
 import seng302.group5.model.Task;
@@ -17,7 +23,7 @@ import seng302.group5.model.Task;
  */
 public class StoryItemController {
 
-  @FXML private ListView<Task> notStartedList;
+  @FXML private VBox notStartedList;
   @FXML private ListView<Task> inProgressList;
   @FXML private ListView<Task> verifyList;
   @FXML private ListView<Task> doneList;
@@ -55,6 +61,9 @@ public class StoryItemController {
       switch (task.getStatus()) {
         case NOT_STARTED:
           notStartedTasks.add(task);
+          // TODO more testing
+          addWithDragging(notStartedList, new Label(task.getLabel()));
+          // TODO this line specifically
           break;
         case IN_PROGRESS:
           inProgressTasks.add(task);
@@ -67,10 +76,76 @@ public class StoryItemController {
           break;
       }
     }
-    notStartedList.setItems(notStartedTasks);
     inProgressList.setItems(inProgressTasks);
     verifyList.setItems(verifyTasks);
     doneList.setItems(doneTasks);
+
+    /**
+     * Testing stuff
+     */
+    // in case user drops node in blank space in root:
+    notStartedList.setOnMouseDragReleased(new EventHandler<MouseDragEvent>() {
+      @Override
+      public void handle(MouseDragEvent event) {
+        int indexOfDraggingNode = notStartedList.getChildren().indexOf(event.getGestureSource());
+        rotateNodes(notStartedList, indexOfDraggingNode, notStartedList.getChildren().size() - 1);
+      }
+    });
+  }
+
+  /**
+   * More testing stuff
+   * @param root
+   * @param label
+   */
+  private void addWithDragging(VBox root, Label label) {
+    label.setOnDragDetected(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        label.startFullDrag();
+      }
+    });
+
+    // next two handlers just an idea how to show the drop target visually:
+    label.setOnMouseDragEntered(new EventHandler<MouseDragEvent>() {
+      @Override
+      public void handle(MouseDragEvent event) {
+        label.setStyle("-fx-background-color: #ffffa0;");
+      }
+    });
+    label.setOnMouseDragExited(new EventHandler<MouseDragEvent>() {
+      @Override
+      public void handle(MouseDragEvent event) {
+        label.setStyle("");
+      }
+    });
+
+    label.setOnMouseDragReleased(new EventHandler<MouseDragEvent>() {
+      @Override
+      public void handle(MouseDragEvent event) {
+        label.setStyle("");
+        int indexOfDraggingNode = root.getChildren().indexOf(event.getGestureSource());
+        int indexOfDropTarget = root.getChildren().indexOf(label);
+        rotateNodes(root, indexOfDraggingNode, indexOfDropTarget);
+        event.consume();
+      }
+    });
+    root.getChildren().add(label);
+  }
+
+  /**
+   * Please dear got let this work
+   * TODO everything
+   * @param root
+   * @param indexOfDraggingNode
+   * @param indexOfDropTarget
+   */
+  private void rotateNodes(final VBox root, final int indexOfDraggingNode,
+                           final int indexOfDropTarget) {
+    if (indexOfDraggingNode >= 0 && indexOfDropTarget >= 0) {
+      final Node node = root.getChildren().remove(indexOfDraggingNode);
+      root.getChildren().add(indexOfDropTarget, node);
+    }
   }
 
   public Story getStory() {
