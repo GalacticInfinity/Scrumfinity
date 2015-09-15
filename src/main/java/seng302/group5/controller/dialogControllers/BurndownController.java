25e81c89cd8d01d04ff8e878376c1c781c635bd1
Craig Barnard <cab183@uclive.ac.nz>
2015-09-15 15:19:54 +1200

    #fix removed some warnings from code #story:356

--- src/main/java/seng302/group5/controller/dialogControllers/BurndownController.java
+++ src/main/java/seng302/group5/controller/dialogControllers/BurndownController.java
@@ -1,26 +1,20 @@
 package seng302.group5.controller.dialogControllers;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seng302.group5.Main;
 import seng302.group5.model.Backlog;
 import seng302.group5.model.Sprint;
 import seng302.group5.model.Story;
-import seng302.group5.model.Task;
 
 /**
  * Created by Craig Barnard on 10/09/2015.
@@ -38,8 +32,6 @@ public class BurndownController {
 
   private ObservableList<Sprint> availableSprints;
   private ObservableList<Backlog> availableBacklogs;
-
-  private Story nonStory;
 
   //TODO Javadoc
 
@@ -63,7 +55,7 @@ public class BurndownController {
   private void initialiseLists() {
     availableSprints = FXCollections.observableArrayList();
     availableBacklogs = FXCollections.observableArrayList();
-    availableBacklogs.addAll(this.mainApp.getBacklogs());;
+    availableBacklogs.addAll(this.mainApp.getBacklogs());
 
     backlogCombo.getSelectionModel().clearSelection();
     backlogCombo.setItems(mainApp.getBacklogs());
@@ -88,21 +80,13 @@ public class BurndownController {
     );
 
     // Resizes the linecharts height when the main stage is resized.
-    stage.heightProperty().addListener(new ChangeListener<Number>() {
-      @Override
-      public void changed(ObservableValue<? extends Number> observable, Number oldValue,
-                          Number newValue) {
-        burndownChart.setMinHeight(stage.getHeight() - 200);
-      }
+    stage.heightProperty().addListener((observable, oldValue, newValue) -> {
+      burndownChart.setMinHeight(stage.getHeight() - 200);
     });
 
     // Resizes the linecharts width when the main stage is resized.
-    stage.widthProperty().addListener(new ChangeListener<Number>() {
-      @Override
-      public void changed(ObservableValue<? extends Number> observable, Number oldValue,
-                          Number newValue) {
-        burndownChart.setMinWidth(stage.getWidth() - 210);
-      }
+    stage.widthProperty().addListener((observable, oldValue, newValue) -> {
+      burndownChart.setMinWidth(stage.getWidth() - 210);
     });
 
     sprintCombo.getSelectionModel().selectedItemProperty().addListener(
@@ -111,16 +95,19 @@ public class BurndownController {
             burndownChart.setTitle(sprintCombo.getSelectionModel().getSelectedItem().toString());
             for (Story story : newSprint.getSprintStories()) {
               if (mainApp.getStories().contains(story)) {
-
+                //TODO set the sprints burndown chart here
               }
             }
 
           }
         }
     );
-    XYChart.Series<Integer, LocalDate> x = new XYChart.Series<>();
+
   }
 
+  /**
+   * Refresh the comboboxes whenever objects are modified in the main app
+   */
   public void refreshComboBoxes() {
     Backlog backlog = backlogCombo.getValue();
     backlogCombo.getSelectionModel().clearSelection();
